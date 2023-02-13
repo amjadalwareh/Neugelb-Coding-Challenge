9afd4e59605eba13e27abb5e378e9486e0cd4a3a
Amjad <a.alwareh25@gmail.com>
2023-02-13 17:57:42 +0100

    Removed unused code.

--- app/src/main/java/com/neugelb/challenge/MainActivity.kt
+++ app/src/main/java/com/neugelb/challenge/MainActivity.kt
@@ -2,17 +2,14 @@ package com.neugelb.challenge
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.neugelb.presentation.movie_details.MovieDetailsFragmentArgs
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-//    private val viewBinding by viewBinding(ActivityMainBinding::inflate)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MovieDetailsFragmentArgs
+
     }
 }
