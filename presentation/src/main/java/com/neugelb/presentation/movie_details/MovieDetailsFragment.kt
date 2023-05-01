367c9fefbdaef94177f3e6bdfacd49de87ce4ce3
Amjad <a.alwareh25@gmail.com>
2023-05-01 19:52:41 +0200

    Upgrade to AGP 8.0

--- presentation/src/main/java/com/neugelb/presentation/movie_details/MovieDetailsFragment.kt
+++ presentation/src/main/java/com/neugelb/presentation/movie_details/MovieDetailsFragment.kt
@@ -5,16 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,11 +76,16 @@ class MovieDetailsFragment : Fragment() {
 
     @Composable
     private fun Loading(modifier: Modifier = Modifier) {
-        CircularProgressIndicator(
-            modifier = modifier.then(
-                Modifier.size(16.dp)
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = modifier.then(
+                    Modifier.size(32.dp)
+                )
             )
-        )
+        }
     }
 
     /**
@@ -88,53 +102,58 @@ class MovieDetailsFragment : Fragment() {
         movieDetails: MovieDetails?
     ) {
 
-        Column {
-            Row {
-                AsyncImage(
-                    modifier = modifier
-                        .height(256.dp)
-                        .padding(8.dp),
-                    model = BuildConfig.IMAGE_BASE_URL + movieDetails?.posterPath,
-                    contentDescription = movieDetails?.originalTitle
+        Surface(
+            modifier = modifier
+        ) {
+            Column {
+                Row {
+                    AsyncImage(
+                        modifier = modifier
+                            .height(256.dp)
+                            .padding(8.dp),
+                        model = BuildConfig.IMAGE_BASE_URL + movieDetails?.posterPath,
+                        contentDescription = movieDetails?.originalTitle
+                    )
+
+                    Text(
+                        modifier = modifier.padding(8.dp),
+                        style = MaterialTheme.typography.h3,
+                        text = movieDetails?.originalTitle.toString(),
+                        color = colorResource(id = R.color.purple_500)
+                    )
+                }
+
+                Text(
+                    modifier = modifier.padding(8.dp),
+                    style = MaterialTheme.typography.subtitle1,
+                    text = movieDetails?.overview.toString(),
+                    color = Color.White
                 )
 
                 Text(
                     modifier = modifier.padding(8.dp),
-                    style = MaterialTheme.typography.h3,
-                    text = movieDetails?.originalTitle.toString()
+                    style = MaterialTheme.typography.body1,
+                    text = movieDetails?.genres?.genresAsString().toString(),
+                    color = Color.White,
+                    fontStyle = FontStyle.Italic
+                )
+
+                Text(
+                    modifier = modifier.padding(8.dp),
+                    style = MaterialTheme.typography.body1,
+                    text = movieDetails?.spokenLanguage?.spokenLanguageAsString().toString(),
+                    color = Color.White,
+                    fontStyle = FontStyle.Italic
+                )
+
+                Text(
+                    modifier = modifier.padding(8.dp),
+                    style = MaterialTheme.typography.body1,
+                    text = movieDetails?.releaseDate.toString(),
+                    color = Color.White,
+                    fontStyle = FontStyle.Italic
                 )
             }
-
-            Text(
-                modifier = modifier.padding(8.dp),
-                style = MaterialTheme.typography.subtitle1,
-                text = movieDetails?.overview.toString(),
-                color = Color.White
-            )
-
-            Text(
-                modifier = modifier.padding(8.dp),
-                style = MaterialTheme.typography.body1,
-                text = movieDetails?.genres?.genresAsString().toString(),
-                color = Color.White,
-                fontStyle = FontStyle.Italic
-            )
-
-            Text(
-                modifier = modifier.padding(8.dp),
-                style = MaterialTheme.typography.body1,
-                text = movieDetails?.spokenLanguage?.spokenLanguageAsString().toString(),
-                color = Color.White,
-                fontStyle = FontStyle.Italic
-            )
-
-            Text(
-                modifier = modifier.padding(8.dp),
-                style = MaterialTheme.typography.body1,
-                text = movieDetails?.releaseDate.toString(),
-                color = Color.White,
-                fontStyle = FontStyle.Italic
-            )
         }
 
     }
