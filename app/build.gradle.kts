d8c85576e58bb804755ed061ca87d28c7bfd923d
Amjad <a.alwareh25@gmail.com>
2024-01-21 18:38:48 +0100

    Upgrade material to 1.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation(project(":data"))
     implementation(Dependencies.hilt_android)
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     val nav_version = "2.5.3"
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
